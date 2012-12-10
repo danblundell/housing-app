05c37f1fac80b1b4577b07460c268f87288552d7
Dan Blundell <danblundell@me.com>
2012-12-10 15:48:41 +0000

    ui and js updates

--- js/site.js
+++ js/site.js
@@ -6,10 +6,18 @@
 		var self = this;
 
 		this.questions = ko.observableArray();
+		this.currentQ = ko.observable(0);
 
 		//move to the next page
-		this.nextQ = function () {
-
+		this.nextQ = function (e) {
+			console.log("next q");
+			if(this.link()){
+				window.location = this.link();
+			} 
+			else{
+				self.currentQ(self.currentQ() + 1);
+			}
+			
 		};
 
 		this.init = function (data) {
@@ -19,10 +27,12 @@
 					answers = questions[i].answers;
 
 				question.content(questions[i].question);
+				question.index(i);
 
 				for (var n = 0; n < answers.length; n += 1) {
 					var answer = new nbcApp.models.Answer();
 					answer.content(answers[n].answer);
+					answer.link(answers[n].link);
 					question.answers.push(answer);
 				};
 
@@ -34,6 +44,7 @@
 
 	nbcApp.models.Question = function () {
 		var self = this;
+		this.index = ko.observable();
 		this.content = ko.observable();
 		this.answers = ko.observableArray();
 	};
@@ -50,15 +61,29 @@
 			{
 				"question" : "Does this work?",
 				"answers" : [
-					{"answer" : "Yes"},
-					{"answer" : "No"}
+					{
+						"answer" : "Yes",
+						"link" : "http://www.northampton.gov.uk"
+					},
+					{
+						"answer" : "No"
+					}
 				]
 			},
 			{
 				"question" : "Does this work too?",
 				"answers" : [
-					{"answer" : "Almost"},
-					{"answer" : "Perfectly"}
+					{
+						"answer" : "Almost",
+						"link" : "http://www.northampton.gov.uk"
+					},
+					{
+						"answer" : "Perfectly"
+					},
+					{
+						"answer" : "Not remotely",
+						"link" : "http://www.northampton.gov.uk/housing"
+					}
 				]
 			}
 		]
